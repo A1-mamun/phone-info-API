bfcaec22b4b16a2f9f81cdb0d0e07879779f6c36
A1-mamun <1154almamun@gmail.com>
2024-02-28 06:22:10 +0600

    phone card images accesed from api

--- scripts/phone.js
+++ scripts/phone.js
@@ -4,7 +4,36 @@ const loadPhone = async () => {
   );
   const data = await res.json();
   const phones = data.data;
-  console.log(phones);
+  // console.log(phones);
+  displayPhones(phones);
+};
+
+const displayPhones = (phones) => {
+  //
+  const phoneContainer = document.getElementById("phone-container");
+
+  phones.forEach((phone) => {
+    // console.log(phone);
+    // create a div
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+    // set inner htnl
+    phoneCard.innerHTML = `
+     <figure class="px-10 pt-10">
+            <img src="${phone.image}" alt="Phone"
+                class="rounded-xl" />
+        </figure>
+        <div class="card-body items-center text-center">
+            <h2 class="card-title">${phone.phone_name}!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+    `;
+    // append child
+    phoneContainer.appendChild(phoneCard);
+  });
 };
 
 loadPhone();
